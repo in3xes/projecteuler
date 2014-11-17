aa272bd7f31daae6b8354be9879403639aeb1963
Pradeepkumar G <in3xes@gmail.com>
2014-11-17 15:39:37 +0530

    Problems for today

--- cheff/template.java
+++ cheff/template.java
@@ -4,7 +4,6 @@ public class Main {
 	java.io.BufferedReader r = new java.io.BufferedReader(new java.io.InputStreamReader (System.in));
     }
 
-    
     public static void prnt(String s) {
 	System.out.println(s);
     }
--- cheff/util.java
+++ cheff/util.java
@@ -49,4 +49,10 @@ public class Main {
 	return false;
     }
 
+    static int GCD(int a, int b) {
+	if(a % b == 0)
+	    return b;
+
+	return GCD(b, a % b);
+    }
 }
