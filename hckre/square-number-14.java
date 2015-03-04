9ec03a750b318a4f7ffcdcf10f0ac007818435e0
Pradeepkumar G <pradeep@hackerearth.com>
2015-03-04 22:41:36 +0530

    Become a better programmer

---
+++ hckre/square-number-14.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws java.lang.Exception {
+	java.io.BufferedReader r = new java.io.BufferedReader(new java.io.InputStreamReader (System.in));
+
+	long n = toLong(r.readLine());
+	long sq = (long) Math.sqrt(n);
+	if(sq * sq == n)
+	    prnt("YES");
+	else
+	    prnt("NO");
+    }
+
+    public static void prnt(String s) {
+	System.out.println(s);
+    }
+    public static void prnt(int s) {
+	System.out.println(s);
+    }
+    public static void prnt(double s) {
+	System.out.println(s);
+    }
+    public static void prnt(long s) {
+	System.out.println(s);
+    }
+    public static void prnt(char s) {
+	System.out.println(s);
+    }
+    public static int toInt(String s) {
+	return Integer.parseInt(s);
+    }
+    public static long toLong(String s) {
+	return Long.parseLong(s);
+    }
+}
